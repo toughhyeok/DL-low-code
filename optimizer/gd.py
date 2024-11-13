25917bb6143a8ecd10ee72f1008e4cb27983b197
toughhyeok <toughhyeok96@gmail.com>
2024-11-13 20:39:34 +0900

    gd.py 이해하기 쉽게 변경하기 (#2)

--- optimizer/gd.py
+++ optimizer/gd.py
@@ -2,15 +2,34 @@ import numpy as np
 
 
 def compute_loss(a, b, X, y):
-    m = len(y)
-    predictions = a * X + b
-    return (1 / (2 * m)) * np.sum((predictions - y) ** 2)
+    theta = np.array([[a], [b]])
+    m = len(y)  # X.shape[0]
+
+    one_vector = np.ones((m, 1))
+    X_prime = np.hstack((X, one_vector))
+
+    return (1 / m) * np.sum((y - X_prime.dot(theta)) ** 2)
 
 
 def compute_gradients(a, b, X, y):
-    m = len(y)
-    da = (1 / m) * np.sum((a * X + b - y) * X)
-    db = (1 / m) * np.sum(a * X + b - y)
+    """
+    Gradient
+    `gradient = -(1 / m) * np.dot(X_prime.T, y - X_prime.dot(theta))`
+
+    Gradient Descent
+    `theta_new = theta - learning_rate * gradient`
+    """
+
+    theta = np.array([[a], [b]])
+    m = len(y)  # X.shape[0]
+
+    one_vector = np.ones((m, 1))
+    X_prime = np.hstack((X, one_vector))
+
+    gradient = -(1 / m) * np.dot(X_prime.T, y - X_prime.dot(theta))
+
+    da, db = gradient[0, 0], gradient[1, 0]
+
     return da, db
 
 
